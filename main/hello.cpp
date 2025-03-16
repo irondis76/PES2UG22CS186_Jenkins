250e2c997b513e2a61233c88cd5b1fbf0d7c3075
irondis76 <dishanthk76@gmail.com>
2025-03-16 13:01:11 +0530

    testing webhook

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,5 +7,6 @@ int main(void) {
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   cout << "It is now running  \n";
+  cout << "SEtup Git Webhook \n";
   return 0;
 }
